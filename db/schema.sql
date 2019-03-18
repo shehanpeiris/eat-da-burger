10a8cfd4586fd86ac8550b807c87af382f886e4e
speiris <shehanpeiris@gmail.com>
2019-03-18 00:25:26 -0500

    merging new project

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+### Schema
+
+DROP DATABASE burger_db;
+CREATE DATABASE burger_db;
+USE burger_db;
+
+CREATE TABLE burgers
+(
+	id INT NOT NULL AUTO_INCREMENT,
+	burger_name VARCHAR(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Au Cheval burger');
+INSERT INTO burgers (burger_name) VALUES ('BBQ burger');
+INSERT INTO burgers (burger_name) VALUES ('double cheeseburger');
